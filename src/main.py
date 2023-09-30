0e33fd8e1ab55689eb02ba834c7f3f16bbcc83f1
Jan-Sliva <honzik.sliva@seznam.cz>
2023-09-30 17:47:08 +0200

    Create first selestion screen

--- src/main.py
+++ src/main.py
@@ -5,8 +5,8 @@ from PyQt5.QtGui import QFont
 import importlib.util
 
 spec = importlib.util.spec_from_file_location("strategy", "strategies/outlineFirst/strategy.py")
-strategy = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(strategy)
+strat = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(strat)
 
 font_size = 12
 window_size = (700, 700)
@@ -20,21 +20,7 @@ app = QApplication(sys.argv)
 window = mainWindow.mainWindow()
 window.setBaseSize(*window_size)
 
-
-strat = strategy.strategy(window, "strategies/outlineFirst")
-
-def sendOutput(text):
-    for k, v in text.items():
-        print(k + ": " + str(v))
-    strat.setFirstPromptScreen()
-    
-
-
-window.setSelectionWithButton([{"key" : "bool1", "textAfter" : "OK", "type" : "boolInput"},
-                               {"key" : "selection1", "textBefore" : "selection", "textAfter" : "letter", "type" : "selectionInput", "items" : ["A", "B", "C"]},
-                               {"key" : "text1", "textBefore" : "Text: ", "textAfter" : "lol", "type" : "textInput"},
-                               {"type" : "text", "text" : "cyrz kacyrz", "fontSize" : 50}], sendOutput)
-
+window.setFirstScreen()
 
 window.resize(*window_size)
 window.show()
--- src/mainWindow.py
+++ src/mainWindow.py
@@ -5,12 +5,15 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QImage, QPen, QTabletEvent, QFont
 from PyQt5.QtWidgets import QLayout
 from selectionLayout import selectionLayout
-
+import importlib.util
+import os.path as P
+import json
 
 class mainWindow(QtWidgets.QMainWindow):
 
     def __init__(self):
         super().__init__()
+        self.strategy = None
 
     def setCentralLayout(self, layout: QLayout) -> None:
         w = QtWidgets.QWidget()
@@ -23,4 +26,54 @@ class mainWindow(QtWidgets.QMainWindow):
         layout.addSelectionButton(selectionOutputs, sendOutput, buttonText)
         layout.addStretch()
         
+        self.setCentralLayout(layout)
+
+    def setStrategy(self, params):
+        spec = importlib.util.spec_from_file_location("strategy", P.join(params["strategyPath"], "strategy.py"))
+        strat = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(strat)
+
+        del self.strategy
+        self.strategy = strat.strategy(self, params["strategyPath"])
+        self.strategy.begin(params)
+        
+    def setFirstScreen(self):
+        layout = selectionLayout()
+
+        strategies_names = []
+        strategies_dict = {}
+
+
+        for dir in os.listdir("strategies"):
+            strat_path = P.join("strategies", dir)
+            info_file = P.join(strat_path, "info.json")
+            with open(info_file, encoding="utf-8") as f:
+                info = json.load(f)
+            strategies_names.append(info["name"])
+            strategies_dict[info["name"]] = {"description" : info["description"], "file" : strat_path}
+
+        strategy_selection_list = [
+            {"type" : "text", "text" : "Set parameters", "fontSize" : 15},
+            {"key" : "strategy", "textBefore" : "selection", "textAfter" : "letter", "type" : "selectionInput", "items" : strategies_names}
+        ]
+
+        desc_label = QLabel()
+        desc_label.setWordWrap(True)
+
+        def updateDesc(params):
+            desc_label.setText("Description of strategy: " + strategies_dict[params["strategy"]]["description"])
+        
+        strategy_selection_outputs = layout.addSelection(strategy_selection_list, updateDesc, triggerOnce=True)
+        layout.addWidget(desc_label)
+
+        param_selecton_list = [
+            {"type" : "textInput", "key" : "title", "textBefore" : "Title: "},
+            {"type" : "textInput", "key" : "subtitle", "textBefore" : "Subtitle: "},
+            {"type" : "textInput", "key" : "folder", "textBefore" : "Folder: "},
+            {"type" : "selectionInput", "key" : "lang", "textBefore" : "Language: ", "items" : ["CZECH", "ENGLISH"]},
+        ]
+
+        layout.addSelectionButton(strategy_selection_outputs, self.setStrategy, "Continue")
+        layout.addStretch()
+
         self.setCentralLayout(layout)
